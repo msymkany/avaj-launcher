6a8b12523caf8990bf0320e8532a91377af78efa
Maria <msymkany@users.noreply.github.com>
2018-06-11 20:26:02 +0300

    simulator edit

--- launcher/src/ua/unit/aircraft/Simulator.java
+++ launcher/src/ua/unit/aircraft/Simulator.java
@@ -1,11 +1,46 @@
 package ua.unit.aircraft;
 
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.LinkedList;
 /**
  * Created by msymkany on 5/26/18.
  */
+
 public class Simulator {
 
-    public static void main(String[] arg){
+	private static WeatherTower weatherTower;
+    private static List<Flyable> flyables = new LinkedList<Flyable>();
+    static PrintWriter printWriter;
+
+    public static void main(String[] args){
+        try {
+            int simulationsNum = 0;
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String line = reader.readLine();
+
+            if (line != null) {
+                simulationsNum = Integer.parseInt(line.split(" ")[0]);
+                if (simulationsNum <= 0) {
+                    throw (new NewException("Invalid number of simulations"));
+                }
+            } else {
+                throw (new NewException("Empty file"));
+            }
+
+
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        catch (NewException e){
+            System.out.println(e.getMessage());
+        }
+
 
 
     }
